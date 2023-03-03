ad8a6725560c53d13ca5f1696f5c1e92f403caa2
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2023-03-03 13:24:56 -0700

    Create For loop with condition
    For loop iterates over each item in pokemonList.
    Inside the loop’s code, Pokémon name on website’s DOM.
    Adding strings to write the Pokémon’s height next to its name with condition highlighting special Pokémon in list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-{name:"Squirtle", types: "water", height: 5},
-{name:"Pikachu", types: "electric", height: 4},
-{name:"Jigglypuff", types: ["fairy", "normal"], height: 5}
+{name:"Squirtle", types: "water", height: 0.5},
+{name:"Pikachu", types: "electric", height: 0.4},
+{name:"Jigglypuff", types: ["fairy", "normal"], height: 0.5}
 ];
+
+for (let i = 0; i < pokemonList.length; i++){
+
+    if (pokemonList[i].height < 0.5) {
+        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's small!");
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's small!"+ "<br>");
+    }
+    else{
+        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")"+ "<br>");
+    }
+}
