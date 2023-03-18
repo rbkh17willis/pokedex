ba56744f0c8b9b294e3ff37ed7e364d07d10208d
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2023-03-17 21:19:38 -0700

    Change for loop to forEach
    forEach function instead of the for loop to iterate over the Pokémon in pokemonList array in order to print the details of each one.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,15 @@ let pokemonList = [
 {name:"Jigglypuff", types: ["fairy", "normal"], height: 0.5}
 ];
 
-for (let i = 0; i < pokemonList.length; i++){
+pokemonList.forEach(function (pokemon) {
 
-    if (pokemonList[i].height < 0.5) {
-        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's small!");
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's small!"+ "<br>");
+    if (pokemon.height < 0.5) {
+        console.log(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's small!");
+        document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's small!"+ "<br>");
     }
     else{
-        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")"+ "<br>");
+        console.log(pokemon.name + " (height: " + pokemon.height + ")");
+        document.write(pokemon.name + " (height: " + pokemon.height + ")"+ "<br>");
     }
-}
+});
+
